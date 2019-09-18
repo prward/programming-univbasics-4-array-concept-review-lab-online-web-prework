00ae1e9a9f2b213d44a00524ec327c7fec8ae2d5
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-18 16:18:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,5 @@
 def find_element_index(array, value_to_find)
-  puts "array.find_index{|e| array.find_index(e)}\n\n"
+  array.find_index{|e| array.find_index(e)}\n\n
 end
 
 def find_max_value(array)

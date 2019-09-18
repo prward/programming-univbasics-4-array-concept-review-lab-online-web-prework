59f86f09bafd7390c693e446356968addbbf1250
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-18 16:27:07 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.find_index{|e| array.find_index(e)}
 end
 
 def find_max_value(array)
-  # Add your solution here
+ array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end

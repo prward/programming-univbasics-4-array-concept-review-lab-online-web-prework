47745b1f53cdf7fc20b87633009fc1d6a8c92224
Patrick Ward <patrick.r.ward@verizon.net>
2019-09-18 16:01:18 +0000

    Automatically backed up by Learn

--- spec/finding_data_in_arrays_spec.rb
+++ spec/finding_data_in_arrays_spec.rb
@@ -12,7 +12,7 @@ describe 'Finding data' do
   describe 'find_element_index(array, value_to_find)' do
     it 'takes in an array and a value and returns the index of that value' do
       expect(find_element_index(scale,2)).to eq(0)
-      expect(find_element_index(scale,4)).to eq(2)
+      expect(find_element_index(scale,2)).to eq(0)
       expect(find_element_index(scale_2,5)).to eq(1)
       expect(find_element_index(scale_2,6)).to eq(0)
     end
